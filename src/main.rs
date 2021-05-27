f07c630324d415287db12994e98e73f54a688065
Mack Stump <mack@mackstump.com>
2021-05-27 13:52:56 -0400

    feat: font asset and define conrod theme

--- src/main.rs
+++ src/main.rs
@@ -64,6 +64,29 @@ fn parse_args() -> Result<CommandLineArgs, String> {
     Ok(cli_args)
 }
 
+/// A set of reasonable stylistic defaults that works for the `gui` below.
+pub fn theme() -> conrod_core::Theme {
+    use conrod_core::position::{Align, Direction, Padding, Position, Relative};
+    conrod_core::Theme {
+        name: "Demo Theme".to_string(),
+        padding: Padding::none(),
+        x_position: Position::Relative(Relative::Align(Align::Start), None),
+        y_position: Position::Relative(Relative::Direction(Direction::Backwards, 20.0), None),
+        background_color: conrod_core::color::DARK_CHARCOAL,
+        shape_color: conrod_core::color::LIGHT_CHARCOAL,
+        border_color: conrod_core::color::BLACK,
+        border_width: 0.0,
+        label_color: conrod_core::color::WHITE,
+        font_id: None,
+        font_size_large: 26,
+        font_size_medium: 18,
+        font_size_small: 12,
+        widget_styling: conrod_core::theme::StyleMap::default(),
+        mouse_drag_threshold: 0.0,
+        double_click_threshold: std::time::Duration::from_millis(500),
+    }
+}
+
 fn main() {
     let args = parse_args();
     if args.is_err() {
@@ -86,7 +109,13 @@ fn main() {
     // window.set_max_fps(60);
 
     // construct our `Ui`.
-    let mut ui = conrod_core::UiBuilder::new([SCREEN_WIDTH as f64, SCREEN_HEIGHT as f64]).build();
+    let mut ui = conrod_core::UiBuilder::new([SCREEN_WIDTH as f64, SCREEN_HEIGHT as f64])
+        .theme(theme())
+        .build();
+
+    // Add a `Font` to the `Ui`'s `font::Map` from file.
+    let font_path = "assets/fonts/NotoSans/NotoSans-Regular.ttf";
+    ui.fonts.insert_from_file(font_path).unwrap();
 
     let mut cursor = [0.0, 0.0]; // Create texture context to perform operations on textures.
 
